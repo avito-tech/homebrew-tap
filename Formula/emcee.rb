2fabf9fcc1457f89e583a79b7ce668b3828595df
Vyacheslav Ishutin <vvishutin@avito.ru>
2024-05-29 19:25:18 +0300

    Update formula for Emcee 21.0.0

--- Formula/emcee.rb
+++ Formula/emcee.rb
@@ -1,25 +1,25 @@
 class Emcee < Formula
   desc "Emcee the tests runner"
   homepage "https://emceetool.io/"
-  version "20.2.0"
+  version "21.0.0"
 
   if OS.mac?
     depends_on :macos => :monterey
 
-    url "https://github.com/avito-tech/Emcee/releases/download/20.2.0/emceeFree.artifact.zip"
-    sha256 "7204b7a5ddd77804b1e4786a13e72c6b85a46acdec6de53fda8379cc0053fe18"
+    url "https://github.com/avito-tech/Emcee/releases/download/21.0.0/homebrew-archive.zip"
+    sha256 "c71dcd2f00515b109af4f86d8f3928d86e3aabe44148453aac712164fbcc55a1"
 
     def install
-      libexec.install "emceeFree", "EmceeTestsInspector"
-      system "xattr", "-c", "#{libexec}/emceeFree"
+      libexec.install "emcee", "emcee-ios", "EmceeTestsInspector", "emcee.jar"
+      system "xattr", "-c", "#{libexec}/emcee"
+      system "xattr", "-c", "#{libexec}/emcee-ios"
       system "xattr", "-c", "#{libexec}/EmceeTestsInspector"
 
-      bin.write_exec_script "#{libexec}/emceeFree"
-      mv "#{bin}/emceeFree", "#{bin}/emcee"
+      bin.write_exec_script "#{libexec}/emcee"
     end
   end
 
   test do
-    system "#{bin}/emcee version"
+    system "#{bin}/emcee --version"
   end
 end
