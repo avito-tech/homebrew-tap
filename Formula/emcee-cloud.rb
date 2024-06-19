4c259e005a1fa151204b92ffd45ffc48bba133e4
Dmitriy Prokop <daprokop@avito.ru>
2024-06-19 16:19:37 +0300

    change emcee.cloud cli download url

--- Formula/emcee-cloud.rb
+++ Formula/emcee-cloud.rb
@@ -1,7 +1,7 @@
 class EmceeCloud < Formula
   desc "Command-Line interface for Emcee.cloud"
   homepage "https://emceetool.io/"
-  url "https://github.com/avito-tech/Emcee.cloud-CLI/releases/download/v0.0.1-beta/emcee-cloud_Darwin_all.tar.gz"
+  url "https://github.com/avito-tech/Emcee.cloud-CLI/releases/download/0.0.1/emcee-cloud_Darwin_all.tar.gz"
   sha256 "1025b2d76923797cbe2d9d08218d39aae94fa69fd05aaaf434d382ebca48545b"
   license "MIT"
   version "0.0.1"
