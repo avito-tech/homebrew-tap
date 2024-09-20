51df16baba2a47255404cc081a4676a22d7704cc
Dmitriy Prokop <daprokop@avito.ru>
2024-09-20 10:59:07 +0300

    emcee.cloud cli 0.0.2

--- Formula/emcee-cloud.rb
+++ Formula/emcee-cloud.rb
@@ -1,11 +1,10 @@
 class EmceeCloud < Formula
   desc "Command-Line interface for Emcee.cloud"
   homepage "https://emceetool.io/"
-  url "https://github.com/avito-tech/Emcee.cloud-CLI/releases/download/0.0.1/emcee-cloud_Darwin_all.tar.gz"
-  sha256 "1025b2d76923797cbe2d9d08218d39aae94fa69fd05aaaf434d382ebca48545b"
+  url "https://github.com/avito-tech/Emcee.cloud-CLI/releases/download/0.0.2/emcee-cloud_Darwin_all.tar.gz"
+  version "0.0.2"
+  sha256 "3402b747c2e8769b8838bec28ef0fc5c48f103b59e97da9aec68296e75060752"
   license "MIT"
-  version "0.0.1"
-
 
   def install
     libexec.install "emcee-cloud"
@@ -13,8 +12,7 @@ class EmceeCloud < Formula
     bin.write_exec_script "#{libexec}/emcee-cloud"
   end
 
- test do
-    system "#{bin}/emcee-cloud --version"
- end
+  test do
+    system "#{bin}/emcee-cloud", "--version"
+  end
 end
-
