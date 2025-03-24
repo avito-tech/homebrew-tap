9794000ca3e17482413b0e6e85fff211e898499e
Vyacheslav Ishutin <vvishutin@avito.ru>
2025-03-24 12:16:12 +0300

    bump Emcee version: 21.10.0

--- Formula/emcee.rb
+++ Formula/emcee.rb
@@ -1,13 +1,13 @@
 class Emcee < Formula
   desc "Emcee the tests runner"
   homepage "https://emceetool.io/"
-  version "21.9.0"
+  version "21.10.0"
 
   if OS.mac?
     depends_on :macos => :monterey
 
-    url "https://github.com/avito-tech/Emcee/releases/download/21.9.0/homebrew-archive.zip"
-    sha256 "54bd92e2885c13203b717c1463fee77448a221cfe109d21455ef8165c62c78c7"
+    url "https://github.com/avito-tech/Emcee/releases/download/21.10.0/homebrew-archive.zip"
+    sha256 "accdcdcf3ac4959dc9c424318e0dc6c0dc74e4a6c56cf6605d2068e71a585dac"
 
     def install
       libexec.install "emcee", "emcee-ios", "EmceeTestsInspector", "emcee.jar"
